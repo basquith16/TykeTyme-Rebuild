3cf47fb2b680b0589de8ecdd6241b054f863eee5
Brian M Asquith <basquithcpt@gmail.com>
2015-12-05 00:20:43 -0500

    routing works, calendar renders but some features do not work and styling is slightly off

--- src/components/sched.jsx
+++ src/components/sched.jsx
@@ -4,9 +4,25 @@ var Link = Router.Link;
 var $ = require('jquery');
 
 module.exports = React.createClass({
-  render: function() {
-    return (
-       <div id = 'calendar' >Hello</div>
-)
-}
+    render: function() {
+      return (
+ <div className="calBody">
+        <div id='wrap'>
+          <div idName='external-events'>
+            <div className='fc-event'>My Event 1</div>
+            <div className='fc-event'>My Event 2</div>
+            <div className='fc-event'>My Event 3</div>
+            <div className='fc-event'>My Event 4</div>
+            <div className='fc-event'>My Event 5</div>
+            <p>
+              <input type='checkbox' id='drop-remove'/>
+              <label for='drop-remove'>remove after drop</label>
+            </p>
+          </div>
+          <div id='calendar'></div>
+          <div></div>
+        </div>
+      </div>
+    )
+  }
 });
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,24 +1,26 @@
-// var React = require('react');
-// var ReactRouter = require('react-router');
-// var HashHistory = require('react-router/lib/hashhistory');
-// var Router = ReactRouter.Router;
-// var Route = ReactRouter.Route;
-// var Main = require('./components/main');
-// var Sched = require('./components/sched');
-// var Crafts = require('./components/crafts');
-// var Activities = require('./components/activities');
-// var Meals = require('./components/meals');
-// var Map = require('./components/map');
-// var Contact = require('./components/contact');
-//
-// module.exports = (
-//   <Router history={new HashHistory}>
-//     <Route path="/" component={Main}></Route>
-//     <Route path="1" component={Sched}></Route>
-//     <Route path="2" component={Crafts}></Route>
-//     <Route path="3" component={Activities}></Route>
-//     <Route path="3" component={Meals}></Route>
-//     <Route path="3" component={Map}></Route>
-//     <Route path="3" component={Contact}></Route>
-//   </Router>
-// )
+var React = require('react');
+var ReactRouter = require('react-router');
+var HashHistory = require('react-router/lib/hashhistory');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Main = require('./components/main');
+var Sched = require('./components/sched');
+var Crafts = require('./components/crafts');
+var Activities = require('./components/activities');
+var Meals = require('./components/meals');
+var Map = require('./components/map');
+var Contact = require('./components/contact');
+
+
+module.exports = (
+  <Router history={new HashHistory}>
+    <Route path="/" component={Main}>
+      <Route path="1" component={Sched} />
+      <Route path="2" component={Crafts} />
+      <Route path="3" component={Activities} />
+      <Route path="3" component={Meals} />
+      <Route path="3" component={Map} />
+      <Route path="3" component={Contact} />
+  </Route>
+  </Router>
+)
